6724c28b8cd7570170ab1e41c36d4c78f790a8e8
Alena Golovina and Mara Timberlake <mara@>
2017-06-06 11:40:41 -0700

    add js loops to replace vowels with-

--- js/scripts.js
+++ js/scripts.js
@@ -2,48 +2,23 @@ $(document).ready(function() {
   $('#submit_button').click(function(event){
   event.preventDefault();
 
-  var wordInput = $("input#word").val();
+  var wordInput = $("input#word").val().toLowerCase();
+  var letterArray = wordInput.split('');
+  // var emptyWordArray = [];
     $("#results").append("Solve the puzzle: " + wordInput);
     $("#form").hide();
     $("h2").hide();
 
     var vowels = ['a', 'e', 'i', 'o', 'u'];
-    var sentence = ($('.sentence').val()).split(''); // is an array
-      for (var i = 0; i < vowels.length; i++) {   // loop through vowels
-
-      }  // for loop
-  });
-
-    //var seperateLetters = wordInput.split();
-    // wordInput.forEach(function(wordInput) {
-    //   for
-    //
-    //   if (separateLetter = "a"){
-    //     $(separateLetter).replace("-")
-    //     alert(wordInput);
-    // }
-    //
-//}
-    //function removeVowels(wordInput) {
-      //return wordInput.replace(/[aeiou]/gi, '-');
-  //  }
-//var seperateLetters = wordInput.split("");
-    //.replace(/[aeiou]/ig,'-')
-
-    // function removeVowels(str) {
-
-
-    // for (i = 0; i<seperateLetters.length; i++) {
-    //   );
-    // }
-
-
-    // var seperateLetters = wordInput.split("");
-    //alert(seperateLetters);
-
-
-
-
+    for (var i = 0; i < vowels.length; i++) {   // loop through vowels
+      for (var j = 0; j < letterArray.length; j++) {
+        if (vowels[i] === letterArray[j]) {
+          console.log(letterArray[j]);
+          wordInput = wordInput.replace(letterArray[j], '-');
+        }
+      }
+    }
+  console.log(wordInput);
 
 
   });
