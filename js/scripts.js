8615b895a0bad6dfe5c25f66b01a3a7912abbdf2
Alena Golovina and Mara Timberlake <alena@>
2017-06-06 11:02:37 -0700

    add scrips.js and index.html, print out alert and hide user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,50 @@
 $(document).ready(function() {
-  
+  $('#submit_button').click(function(event){
+  event.preventDefault();
+
+  var wordInput = $("input#word").val();
+    $("#results").append("Solve the puzzle: " + wordInput);
+    $("#form").hide();
+    $("h2").hide();
+
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    var sentence = ($('.sentence').val()).split(''); // is an array
+      for (var i = 0; i < vowels.length; i++) {   // loop through vowels
+
+      }  // for loop
+  });
+
+    //var seperateLetters = wordInput.split();
+    // wordInput.forEach(function(wordInput) {
+    //   for
+    //
+    //   if (separateLetter = "a"){
+    //     $(separateLetter).replace("-")
+    //     alert(wordInput);
+    // }
+    //
+//}
+    //function removeVowels(wordInput) {
+      //return wordInput.replace(/[aeiou]/gi, '-');
+  //  }
+//var seperateLetters = wordInput.split("");
+    //.replace(/[aeiou]/ig,'-')
+
+    // function removeVowels(str) {
+
+
+    // for (i = 0; i<seperateLetters.length; i++) {
+    //   );
+    // }
+
+
+    // var seperateLetters = wordInput.split("");
+    //alert(seperateLetters);
+
+
+
+
+
+
+  });
 });
